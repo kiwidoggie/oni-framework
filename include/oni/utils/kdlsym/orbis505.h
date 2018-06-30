44e24d415989a80f79ea3115203610d59c159bb8
kiwidog <forums@allenthinks.com>
2018-06-30 13:57:15 -0700

    Fix build issue due to missing strstr for 5.05

--- include/oni/utils/kdlsym/orbis505.h
+++ include/oni/utils/kdlsym/orbis505.h
@@ -92,7 +92,8 @@ for the platforms that do enable kernel ASLR (Address Space Layout Randomization
 #define kdlsym_addr_self_orbis_sysvec		0x019bbcd0
 #define kdlsym_addr_trap_fatal				0x00171580
 #define	kdlsym_addr_memcmp                  0x50ac0
-#define	kdlsym_addr__mtx_unlock_sleep                          0x4020a0
-#define	kdlsym_addr__mtx_lock_sleep                            0x401d70
+#define	kdlsym_addr__mtx_unlock_sleep       0x4020a0
+#define	kdlsym_addr__mtx_lock_sleep         0x401d70
+#define kdlsym_addr_strstr					0x17DFB0
 #endif
 
