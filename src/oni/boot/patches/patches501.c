873d795e8fd7812b8e13a7032390a2cf4c8fc931
Specter <cryptogenicdev@gmail.com>
2018-08-14 22:11:38 -0400

    Update patches501.c

--- src/oni/boot/patches/patches501.c
+++ src/oni/boot/patches/patches501.c
@@ -59,15 +59,14 @@ void install_prerunPatches_501()
 
 	// Don't restrict dynlib information. 
 	kmem = (uint8_t *)&gKernelBase[0x002B2350];
-
-        kmem[0] = 0x90;
-        kmem[1] = 0x90;
-        kmem[2] = 0x90;
-        kmem[3] = 0x90;
-        kmem[4] = 0x90;
-        kmem[5] = 0xC3;
-        kmem[6] = 0xC0;
-        kmem[7] = 0x31;
+	kmem[0] = 0x90;
+	kmem[1] = 0x90;
+	kmem[2] = 0x90;
+	kmem[3] = 0x90;
+	kmem[4] = 0x90;
+	kmem[5] = 0xC3;
+	kmem[6] = 0xC0;
+	kmem[7] = 0x31;
 
     	// Allow usage of mangled symbols in dynlib_do_dlsym()
      	kmem = (uint8_t *)&gKernelBase[0x002AF877];
