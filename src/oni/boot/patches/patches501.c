98e9b03bdde8b965725ee2196220ab31849b6dcf
Specter <cryptogenicdev@gmail.com>
2018-08-14 21:51:03 -0400

    Update patches501.c

--- src/oni/boot/patches/patches501.c
+++ src/oni/boot/patches/patches501.c
@@ -2,6 +2,10 @@
 #include <oni/utils/kdlsym.h>
 #include <oni/utils/cpu.h>
 
+/*
+	Please, please, please!
+	Keep patches consistent with the used patch style for readability.
+*/
 void install_prerunPatches_501()
 {
 	// You must assign the kernel base pointer before anything is done
@@ -9,28 +13,52 @@ void install_prerunPatches_501()
 		return;
 
 	void(*critical_enter)(void) = kdlsym(critical_enter);
-	void(*crtical_exit)(void) = kdlsym(critical_exit);
+	void(*crtical_exit)(void)   = kdlsym(critical_exit);
 
 	// Apply patches
 	critical_enter();
 	cpu_disable_wp();
+	
+	// Use "kmem" for all patches
+	uint8_t *kmem;
 
-	// enable UART
-	gKernelBase[0x09ECAE0] = 0;
+	// Enable UART
+	kmem = (uint8_t *)&gKernelBase[0x009ECAE0];
+	kmem[0] = 0x00;
 
 	// Verbose Panics
-	uint8_t *kmem = (uint8_t *)&gKernelBase[0x171517];
-	kmem[0] = 0x90; kmem[1] = 0x90; kmem[2] = 0x90; kmem[3] = 0x90;
-	kmem[4] = 0x90; kmem[5] = 0x65; kmem[6] = 0x8B; kmem[7] = 0x34;
-	kmem = (uint8_t *)&gKernelBase[0x11730];
-	kmem[0] = 0xB8; kmem[1] = 0x01; kmem[2] = 0x00; kmem[3] = 0x00;
-	kmem[4] = 0x00; kmem[5] = 0xC3; kmem[6] = 0x90; kmem[7] = 0x90;
-	kmem = (uint8_t *)&gKernelBase[0x11750];
-	kmem[0] = 0xB8; kmem[1] = 0x01; kmem[2] = 0x00; kmem[3] = 0x00;
-	kmem[4] = 0x00; kmem[5] = 0xC3; kmem[6] = 0x90; kmem[7] = 0x90;
+	kmem = (uint8_t *)&gKernelBase[0x00171517];
+	kmem[0] = 0x90;
+	kmem[1] = 0x90;
+	kmem[2] = 0x90;
+	kmem[3] = 0x90;
+	kmem[4] = 0x90;
+	kmem[5] = 0x65;
+	kmem[6] = 0x8B;
+	kmem[7] = 0x34;
+	
+	kmem = (uint8_t *)&gKernelBase[0x00011730];
+	kmem[0] = 0xB8;
+	kmem[1] = 0x01;
+	kmem[2] = 0x00;
+	kmem[3] = 0x00;
+	kmem[4] = 0x00;
+	kmem[5] = 0xC3;
+	kmem[6] = 0x90;
+	kmem[7] = 0x90;
+	
+	kmem = (uint8_t *)&gKernelBase[0x00011750];
+	kmem[0] = 0xB8;
+	kmem[1] = 0x01;
+	kmem[2] = 0x00;
+	kmem[3] = 0x00;
+	kmem[4] = 0x00;
+	kmem[5] = 0xC3;
+	kmem[6] = 0x90;
+	kmem[7] = 0x90;
 
 	// Don't restrict dynlib information. 
-	kmem = (uint8_t*)&gKernelBase[0x2B2350];
+	kmem = (uint8_t *)&gKernelBase[0x002B2350];
 
         kmem[0] = 0x90;
         kmem[1] = 0x90;
@@ -41,46 +69,49 @@ void install_prerunPatches_501()
         kmem[6] = 0xC0;
         kmem[7] = 0x31;
 
-    // Allow usage of mangled symbols in dynlib_do_dlsym(). 
+    	// Allow usage of mangled symbols in dynlib_do_dlsym()
+     	kmem = (uint8_t *)&gKernelBase[0x002AF877];
+     	kmem[0] = 0x90;
+     	kmem[1] = 0x90;
+     	kmem[2] = 0x90;
+     	kmem[3] = 0x90;
+     	kmem[4] = 0x90;
+     	kmem[5] = 0x90;
 	
-     kmem = (uint8_t*)&gKernelBase[0x2AF877];
-
-     kmem[0] = 0x90;
-     kmem[1] = 0x90;
-     kmem[2] = 0x90;
-     kmem[3] = 0x90;
-     kmem[4] = 0x90;
-     kmem[5] = 0x90;
+	// Enable rwx mapping
+	kmem = (uint8_t *)&gKernelBase[0x000FCC38];
+	kmem[0] = 0x07;
 	
-	// Enable rwx
-	kmem = (uint8_t*)&gKernelBase[0xFCC38];
-	kmem[0] = 0x07;
-	kmem = (uint8_t*)&gKernelBase[0xFCC46];
+	kmem = (uint8_t *)&gKernelBase[0x000FCC46];
 	kmem[0] = 0x07;
 
-	// Patch copy(in/out) ?
-	uint16_t *copyinpatch = (uint16_t*)&gKernelBase[0x1EA657];
-	uint16_t *copyoutpatch = (uint16_t*)&gKernelBase[0x1EA572];
-
-	*copyinpatch = 0x9090;
-	*copyoutpatch = 0x9090;
+	// Patch copyin/copyout to allow userland + kernel addresses in both params
+	kmem = (uint8_t *)&gKernelBase[0x001EA657];
+	kmem[0] = 0x90;
+	kmem[1] = 0x90;
+	
+	kmem = (uint8_t *)&gKernelBase[0x001EA572];
+	kmem[0] = 0x90;
+	kmem[1] = 0x90;
 
 	// Enable MAP_SELF
-	kmem = (uint8_t*)&gKernelBase[0x117b0];
+	kmem = (uint8_t *)&gKernelBase[0x000117b0];
 	kmem[0] = 0xB8;
 	kmem[1] = 0x01;
 	kmem[2] = 0x00;
 	kmem[3] = 0x00;
 	kmem[4] = 0x00;
 	kmem[5] = 0xC3;
-	kmem = (uint8_t*)&gKernelBase[0x117c0];
+	
+	kmem = (uint8_t *)&gKernelBase[0x000117c0];
 	kmem[0] = 0xB8;
 	kmem[1] = 0x01;
 	kmem[2] = 0x00;
 	kmem[3] = 0x00;
 	kmem[4] = 0x00;
 	kmem[5] = 0xC3;
-	kmem = (uint8_t*)&gKernelBase[0x13ef2f];
+	
+	kmem = (uint8_t *)&gKernelBase[0x0013ef2f];
 	kmem[0] = 0x31;
 	kmem[1] = 0xC0;
 	kmem[2] = 0x90;
@@ -88,29 +119,34 @@ void install_prerunPatches_501()
 	kmem[4] = 0x90;
 
 	// Patch copyinstr
-	gKernelBase[0x001EAA83] = 0x90;
-	gKernelBase[0x001EAA84] = 0x90;
+	kmem = (uint8_t *)&gKernelBase[0x001EAA83];
+	kmem[0] = 0x90;
+	kmem[1] = 0x90;
 
-	gKernelBase[0x001EAAB3] = 0x90;
-	gKernelBase[0x001EAAB4] = 0x90;
+	kmem = (uint8_t *)&gKernelBase[0x001EAAB3];
+	kmem[0] = 0x90;
+	kmem[1] = 0x90;
 
 	// Patch memcpy stack
-	gKernelBase[0x001EA42D] = 0xEB;
+	kmem = (uint8_t *)&gKernelBase[0x001EA42D];
+	kmem[0] = 0xEB;
 
 	// ptrace patches
-	gKernelBase[0x0030D633] = 0x90;
-	gKernelBase[0x0030D633 + 1] = 0x90;
-	gKernelBase[0x0030D633 + 2] = 0x90;
-	gKernelBase[0x0030D633 + 3] = 0x90;
-	gKernelBase[0x0030D633 + 4] = 0x90;
-	gKernelBase[0x0030D633 + 5] = 0x90;
+	kmem = (uint8_t *)&gKernelBase[0x0030D633];
+	kmem[0] = 0x90;
+	kmem[1] = 0x90;
+	kmem[2] = 0x90;
+	kmem[3] = 0x90;
+	kmem[4] = 0x90;
+	kmem[5] = 0x90;
 
 	// setlogin patch (for autolaunch check)
-	gKernelBase[0x5775C] = 0x48;
-	gKernelBase[0x5775C + 1] = 0x31;
-	gKernelBase[0x5775C + 2] = 0xC0;
-	gKernelBase[0x5775C + 3] = 0x90;
-	gKernelBase[0x5775C + 4] = 0x90;
+	kmem = (uint8_t *)&gKernelBase[0x0005775C];
+	kmem[0] = 0x48;
+	kmem[1] = 0x31;
+	kmem[2] = 0xC0;
+	kmem[3] = 0x90;
+	kmem[4] = 0x90;
 
 	cpu_enable_wp();
 	crtical_exit();
