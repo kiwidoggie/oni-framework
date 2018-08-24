b2a3f85a412da67f75edadd7ddd647a7b7d28f48
kiwidoggie <forums@allenthinks.com>
2018-08-23 17:42:28 -0700

    Fix build errors

--- src/oni/boot/patches/patches455.c
+++ src/oni/boot/patches/patches455.c
@@ -23,7 +23,7 @@ void install_prerunPatches_455()
 	void(*critical_exit)(void) = kdlsym(critical_exit);
 
 	// Apply patches
-	critical_exit();
+	critical_enter();
 	cpu_disable_wp();
 
 	// Use "kmem" for all patches
